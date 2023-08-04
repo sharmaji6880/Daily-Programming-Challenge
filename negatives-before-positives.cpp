8dba3571b8b704c96f2f0c7b23d04efa576d6e86
Raghav Sharma <103110504+sharmaji6880@users.noreply.github.com>
2023-08-04 21:19:27 +0530

    Create negatives-before-positives.cpp
    Day 4 of 30

---
+++ negatives-before-positives.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void func(vector<int> &arr) {
+    vector<int> a,b;
+    for(auto x:arr) {
+        if(x<0) {
+            a.push_back(x);
+        }
+        else {
+            b.push_back(x);
+        }
+    }
+    arr={};
+    for(auto x:a) {
+        arr.push_back(x);
+    }
+    for(auto x:b) {
+        arr.push_back(x);
+    }
+}
+int main() {
+    vector<int> arr={0, 0, 0, -1, -2, -3};
+    func(arr);
+    for(auto x:arr) {
+        cout<<x<<" ";
+    }
+    return 0;
+}
