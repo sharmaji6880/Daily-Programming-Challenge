5213a733d3bbd47060eb5778a52d5f87104d5b2b
Raghav Sharma <103110504+sharmaji6880@users.noreply.github.com>
2023-08-05 21:35:32 +0530

    Create Day_5.cpp

---
+++ Day_5.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int returnCount(vector<int> &arr,int k) {
+    int count=0;
+    int n=arr.size();
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++) {
+            if(arr[i]+arr[j]==k) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+int main() {
+    int k=6;
+    vector<int> arr={1, 2, 3, 4, 5};
+    cout<<returnCount(arr,k);
+    return 0;
+}
