c974d02a5abae6d96dc1c18e8f6618bc7b00cc82
Raghav Sharma <103110504+sharmaji6880@users.noreply.github.com>
2023-08-06 13:39:05 +0530

    Create Day_2.cpp

---
+++ Day_2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int count(vector<int> &arr,int x){
+    int ans=0;
+    for(auto a:arr) {
+        if(a==x) {
+            ans++;
+        }
+        else if(a>x) {
+            return ans;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    vector<int> arr={-10,-10,-5,-5,-5,0,1,1,1,2,3,10,10,10};
+    int x=10;
+    cout<<"Number of occurences of "<<x<<" in arr are:"<<count(arr,x);
+    return 0;
+}
