490c5c51fffb27ff9e977fcf66705d813d668542
Raghav Sharma <103110504+sharmaji6880@users.noreply.github.com>
2023-08-13 13:05:49 +0530

    Create Day_12.cpp

---
+++ Day_12.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <stack>
+using namespace std;
+
+bool isBalanced(string &s) {
+    if(s.size()==0) {
+        return true;
+    }
+    if(s[0]==')' || s[0]==']' || s[0]=='}') {
+        return false;
+    }
+    stack<char> st;
+    map<char,char> m= {
+        {'(',')'},
+        {'[',']'},
+        {'{','}'}
+    };
+    for(int i=0;i<(int)s.size();i++) {
+        if(s[i]=='(' || s[i]=='[' || s[i]=='{') {
+            st.push(s[i]);
+        }
+        else {
+            if(st.empty()) {
+                return false;
+            }
+            if(m[st.top()]==s[i]) {
+                st.pop();
+            }
+        }
+    }
+    if(st.empty()) {
+        return true;
+    }
+    return false;
+}
+
+int main() {
+    string s;
+    cout<<"Enter the string you want to check for balanced parantheses:\n";
+    cin>>s;
+    if(isBalanced(s)) {
+        cout<<"\nBalanced";
+    }
+    else {
+        cout<<"\nNot Balanced";
+    }
+    return 0;
+}
