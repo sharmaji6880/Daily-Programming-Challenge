e8157818d71fbbf86781cd1492149a7c6d2e4ab3
Raghav Sharma <103110504+sharmaji6880@users.noreply.github.com>
2023-08-17 18:06:16 +0530

    Add files via upload

---
+++ Day_17.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+bool isFibonacci(int n) {
+	if(n==1) {
+		return true;
+	}
+	int a=1,b=1;
+	while(b<n) {
+		b=a+b;
+		a=b-a;
+	}
+	if(b==n) {
+		return true;
+	}
+	return false;
+}
+
+int main() {
+	int n;
+	cout<<"Enter the number you want to check for a Fibonacci number:\n";
+    cin>>n;
+    if(isFibonacci(n)) {
+      cout<<n<<" is a member of the Fibonacci sequence";
+    }else {
+      cout<<n<<" is not a member of the Fibonacci sequence";
+    }
+	return 0;
+}
