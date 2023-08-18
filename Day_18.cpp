ed013351c711cd572d052bff81355dd08f15940e
Raghav Sharma <103110504+sharmaji6880@users.noreply.github.com>
2023-08-18 23:25:34 +0530

    Add files via upload

---
+++ Day_18.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int nthRoot(int n,int m) {
+	float x=1.0/n;
+	float ans=pow(m,x);
+	if(ans-floor(ans) == 0) {
+		return (int)ans;
+	}
+	return -1;
+}
+
+int main() {
+	int m,n;
+	cout<<"Enter n:\n";
+	cin>>n;
+	cout<<"Enter m:\n";
+	cin>>m;
+	int x=nthRoot(n,m);
+	if(x!=-1) {
+		cout<<"nth root of "<<m<<" is:\n";
+		cout<<x<<endl;
+	}
+	else {
+		cout<<"nth root of "<<m<<" is not an integer\n";
+	}
+	
+	return 0;
+}
