087cdbad9dc231e7b6af01d046c37aaf5c4bc733
Raghav Sharma <103110504+sharmaji6880@users.noreply.github.com>
2023-08-25 23:10:36 +0530

    Add files via upload

---
+++ Day_24.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> firstNegative(vector<int> arr,int k) {
+	vector<int> ans;
+	int n=arr.size();
+	int neg=0;
+	for(int i=0;i<k;i++) {
+		if(arr[i]<0) {
+			neg=arr[i];
+			break;
+		}
+	}
+	ans.push_back(neg);
+	int left=1,right=k;
+	while(right!=n) {
+		if(arr[left-1]>=0) {
+			if(neg<0) {
+				ans.push_back(neg);
+			}else {
+				if(arr[right]<0) {
+					neg=arr[right];
+				}
+				ans.push_back(neg);
+			}
+			left++;
+			right++;
+		}else if(arr[left-1]<0) {
+			neg=0;
+			for(int j=left;j<=right;j++) {
+				if(arr[j]<0) {
+					neg=arr[j];
+					break;
+				}
+			}
+			ans.push_back(neg);
+			left++;
+			right++;
+		}
+	}
+	return ans;
+}
+
+int main() {
+	int n,k;
+	cout<<"Enter size of the array:\n";
+	cin>>n;
+	vector<int> arr(n);
+	cout<<"Enter the elements of the array:\n";
+	for(int i=0;i<n;i++) {
+		cin>>arr[i];
+	}
+	cout<<"Enter k:\n";
+	cin>>k;
+	vector<int> ans=firstNegative(arr,k);
+	cout<<"The first negative for each window of size "<<k<<" are:\n";
+	for(auto x:ans) {
+		cout<<x<<" ";
+	}
+	return 0;
+}
